3def7de45e6710315dfc7b01cb94daed1df538dc
Kainar <toleubaevkai@mail.ru>
2020-08-11 00:49:30 +0600

    seventh day

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@ $('.overview__carousel').slick({
     autoplay: true,
     autoplaySpeed: 4000
 });
+
+
+//Hamburger
+
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.header__menu'),
+            menuItem = document.querySelectorAll('.header__item'),
+            hamburger = document.querySelector('.hamburger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('header__menu_active');
+            })
+        })
+    });
+
+    
